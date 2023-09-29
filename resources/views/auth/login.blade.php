79d84f38440a2e06957a58ea68674cda1a1902b2
pramono <pramono6236@gmail.com>
2023-09-29 10:43:06 +0700

    fix bugs login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,17 +97,17 @@
   </body>
 </html>
 <script>
-    if ('serviceWorker' in navigator ) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/saleproposmajed/service-worker.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            });
-        });
-    }
+    // if ('serviceWorker' in navigator ) {
+    //     window.addEventListener('load', function() {
+    //         navigator.serviceWorker.register('/saleproposmajed/service-worker.js').then(function(registration) {
+    //             // Registration was successful
+    //             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    //         }, function(err) {
+    //             // registration failed :(
+    //             console.log('ServiceWorker registration failed: ', err);
+    //         });
+    //     });
+    // }
 </script>
 <script type="text/javascript">
     $('.admin-btn').on('click', function(){
