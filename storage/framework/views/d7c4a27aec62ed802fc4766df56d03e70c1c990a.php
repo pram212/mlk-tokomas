accf2bc4b47cb632c014778d3fab58d93c790797
Melkhior Teknologi <mlk.mobdev@gmail.com>
2023-09-29 13:30:11 +0700

    ignore storage folder

---
+++ storage/framework/views/d7c4a27aec62ed802fc4766df56d03e70c1c990a.php
@@ -0,0 +1,3 @@
+<h1>Congratulation <?php echo e($name); ?>!</h1>
+<h3>Hope that our deal will be prosperous</h3>
+<p>Thank you</p><?php /**PATH /home/u539087842/domains/fabuce.co/public_html/bosdemo/resources/views/mail/supplier_create.blade.php ENDPATH**/ ?>
