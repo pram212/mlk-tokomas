516c721b0f3a838fffbda72ba4f57cd5e82194d4
pramono6236 <pramono6236@gmail.com>
2024-02-29 17:14:43 +0700

    hapus cache git

--- resources/views/mail/customer_create.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Congratulation {{$name}}!</h1>
-<h3>Hope that our service will satisfy you</h3>
-<p>Thank you</p>
--- resources/views/mail/holiday_approve.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>Congratulation {{$name}}!</h1>
-<h3>Your requested holiday has just approved.</h3>
-<p>Thank you</p>
