de07a94a359155ebabcf8874f39d78cdc67fa591
Rayhan Putra <=>
2024-11-13 14:00:34 +0700

    hide menu customer group

--- resources/views/sale/partials/pos_modal.blade.php
+++ resources/views/sale/partials/pos_modal.blade.php
@@ -79,14 +79,14 @@
             <div class="modal-body">
                 <p class="italic"><small>{{trans('file.The field labels marked with * are required input
                         fields')}}.</small></p>
-                <div class="form-group">
+                {{-- <div class="form-group">
                     <label>{{trans('file.Customer Group')}} *</strong> </label>
                     <select required class="form-control selectpicker" name="customer_group_id">
                         @foreach($customer_group_all as $customer_group)
                         <option value="{{$customer_group->id}}">{{$customer_group->name}}</option>
                         @endforeach
                     </select>
-                </div>
+                </div> --}}
                 <div class="form-group">
                     <label>{{trans('file.name')}} *</strong> </label>
                     <input type="text" name="customer_name" required class="form-control">
@@ -98,15 +98,15 @@
                 <div class="form-group">
                     <label>{{trans('file.Phone Number')}} *</label>
                     <input type="text" name="phone_number" required class="form-control">
-                </div>
+                </div> --}}
                 <div class="form-group">
                     <label>{{trans('file.Address')}} *</label>
                     <input type="text" name="address" required class="form-control">
-                </div> --}}
-                <div class="form-group">
+                </div>
+                {{-- <div class="form-group">
                     <label>{{trans('file.City')}} *</label>
                     <input type="text" name="city" required class="form-control">
-                </div>
+                </div> --}}
                 <div class="form-group">
                     <input type="hidden" name="pos" value="1">
                     <input type="submit" value="{{trans('file.submit')}}" class="btn btn-primary">
